838b976d90ae50a79bcb8340303295e576951e0f
BouncyButton <48158116+BouncyButton@users.noreply.github.com>
2019-04-12 08:44:00 +0200

    Minor updates

--- G24HM2.py
+++ G24HM2.py
@@ -5,47 +5,42 @@ import sys
 import os
 import time
 import random as rand
+from collections import Counter
 
 
 # 2. MapReduce Word count
 # Improved Word count 1
-def word_count1(sc, docs):
+def word_count1(docs):
     def f1(document):
-        pairs_dict = {}
-        for word in document.split(' '):
-            if word not in pairs_dict.keys():
-                pairs_dict[word] = 1
-            else:
-                pairs_dict[word] += 1
+        # We replaced the dictionary creation with the faster Counter implementation
+        pairs_dict = Counter(document.split(' '))
         return [(key, pairs_dict[key]) for key in pairs_dict.keys()]
 
     # flatMap(f1) is the map phase
-    # groupByKey().map(f2) has been replaced by reduceByKey, which also compute the shuffle
+    # groupByKey().map(f2) has been replaced by reduceByKey, which also computes the shuffle
     word_count_pairs = docs.flatMap(f1).reduceByKey(lambda x, y: x + y)
     # data must be small enough to fit in driver's memory
     return word_count_pairs
 
 
-def word_count2_random(sc, docs, K):
+def word_count2_random(docs, K):
     def f1(document):
-        pairs_dict = {}
-        for word in document.split(' '):
-            if word not in pairs_dict.keys():
-                pairs_dict[word] = 1
-            else:
-                pairs_dict[word] += 1
+        pairs_dict = Counter(document.split(' '))
         return [(key, pairs_dict[key]) for key in pairs_dict.keys()]
 
     def f3(grouped_key_value_list):
         pairs_dict = {}                                     # Dictionary (key=w, value=C(w, x))
-        for word, occ in list(grouped_key_value_list)[0]:   # Iterate through a single partition with key x = [0, k)
+        # Iterate through a single partition with key x = [0, k)
+        # We extract the only element in grouped_key_value_list (a partition)
+        for word, occ in list(grouped_key_value_list)[0]:
             if word not in pairs_dict.keys():
                 pairs_dict[word] = occ
             else:
                 pairs_dict[word] += occ
         return [(key, pairs_dict[key]) for key in pairs_dict.keys()]
 
-    word_count_pairs = (docs
+    word_count_pairs = (
+        docs
         .flatMap(f1)                                # Map_round_1: Produce intermediate pairs (w, Ci(w))
         .groupBy(lambda x: rand.randrange(0, K))    # Map_round_1: Produce intermediate pairs (x, [(w, Ci(w))])
         .groupByKey().flatMapValues(f3).values()    # Reduce_round_1: Produce intermediate pairs (w, c(w, x))
@@ -54,9 +49,10 @@ def word_count2_random(sc, docs, K):
     return word_count_pairs
 
 
-def word_count2_partitioned(sc, docs, K):
+def word_count2_partitioned(docs):
     def f1(documents):
         pairs_dict = {}
+
         for document in documents:
             for word in document.split(' '):
                 if word not in pairs_dict.keys():
@@ -77,7 +73,6 @@ def G24HM2():
     # Import the Dataset
     docs_txt = []
 
-    print(sys.argv)
     assert len(sys.argv) == 3, "Usage: python G24HM2.py <K> <file_name>"
 
     K = sys.argv[1]
@@ -89,7 +84,7 @@ def G24HM2():
     K = int(K)
 
     # Spark Setup
-    conf = SparkConf().setAppName('Word count 1-2').setMaster("local")
+    conf = SparkConf().setAppName('Word count 1-2').setMaster("local[*]")
     sc = SparkContext(conf=conf)
 
     # Create a parallel collection
@@ -100,19 +95,21 @@ def G24HM2():
     # Random K partitions.
     docs.repartition(numPartitions=K)
 
+    # system cores
+    # print(sc.defaultParallelism)
     # Force data loading
     print("Data found: ", docs.count(), " row(s).")
 
     begin_wordcount1 = time.time()
-    word_count1(sc, docs).collect()
+    word_count1(docs).collect()
     print("Elapsed time for Word Count 1: ", time.time() - begin_wordcount1, "second(s).")
 
     begin_wordcount2_random = time.time()
-    word_count2_random(sc, docs, K).collect()
+    word_count2_random(docs, K).collect()
     print("Elapsed time for Word Count 2 random: ", time.time() - begin_wordcount2_random, "second(s).")
 
     begin_wordcount2_partitioned = time.time()
-    word_count = word_count2_partitioned(sc, docs, K)
+    word_count = word_count2_partitioned(docs)
     word_count.collect()
     print("Elapsed time for Word Count 2 partitioned: ", time.time() - begin_wordcount2_partitioned, "second(s).")
 
